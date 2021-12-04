16ad02fd3e9641dbbb020cf76a5ffcc3c7762909
xkerria <xkerria@126.com>
2021-12-04 16:25:56 +0800

    实现阿里云 OSS 文件驱动

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/xkerria/go-storage
 
 go 1.17
+
+require (
+	github.com/aliyun/aliyun-oss-go-sdk v2.2.0+incompatible // indirect
+	github.com/qiniu/go-sdk/v7 v7.11.0 // indirect
+	golang.org/x/time v0.0.0-20211116232009-f0f3c7e86c11 // indirect
+)
--- storage/storage.go
+++ storage/storage.go
@@ -9,9 +9,9 @@ type Storage interface {
 	Put(key string, r io.Reader, dataLength int64) error
 	PutFile(key string, localFile string) error
 	Get(key string) (io.ReadCloser, error)
+	Exists(key string) (bool, error)
 	Rename(srcKey string, destKey string) error
 	Copy(srcKey string, destKey string) error
-	Exists(key string) (bool, error)
 	Size(key string) (int64, error)
 	Delete(key string) error
 	Url(key string) string
